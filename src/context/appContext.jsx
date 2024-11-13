37a41dcceb3adb1bab4d321afe038eafaed355aa
Kanageshwarivelmurugan <vkanageshwari73@gmail.com>
2024-11-13 18:00:18 +0530

    fourthcommit

--- src/context/appContext.jsx
+++ src/context/appContext.jsx
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 import React from "react";
-const SOCKET_URL = "http://localhost:3000";
+const SOCKET_URL = "https://lightningchat-backend.onrender.com";
 export const socket = io(SOCKET_URL);
 export const AppContext = React.createContext();
