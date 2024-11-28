5ce9cd69da42271d82bf258876f3387682fa9bbe
Kanageshwarivelmurugan <vkanageshwari73@gmail.com>
2024-11-28 17:34:34 +0530

    urlchange

--- src/services/appApi.jsx
+++ src/services/appApi.jsx
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const appApi = createApi({
     reducerPath: "appApi",
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:3000",
+        baseUrl: "https://lightningchat-backend.onrender.com",
     }),
 
     endpoints: (builder) => ({
